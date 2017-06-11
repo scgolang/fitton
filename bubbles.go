a9af75816bd107304ff7a42634c8f4b5fb8c36ff
Brian Sorahan <bsorahan@gmail.com>
2017-06-11 06:46:13 -0500

    avoid var block in bubbles synthdef

--- bubbles.go
+++ bubbles.go
@@ -3,22 +3,21 @@ package main
 import "github.com/scgolang/sc"
 
 func bubbles(params sc.Params) sc.Ugen {
-	var (
-		saw2 = sc.LFSaw{
-			Freq:   sc.Multi(sc.C(8), sc.C(7.23)),
-			Iphase: sc.C(0),
-		}.Rate(sc.AR).MulAdd(sc.C(3), sc.C(80))
+	saw2 := sc.LFSaw{
+		Freq:   sc.Multi(sc.C(8), sc.C(7.23)),
+		Iphase: sc.C(0),
+	}.Rate(sc.AR).MulAdd(sc.C(3), sc.C(80))
 
-		saw1 = sc.LFSaw{
-			Freq:   sc.C(0.4),
-			Iphase: sc.C(0),
-		}.Rate(sc.AR).MulAdd(sc.C(24), saw2).Midicps()
+	saw1 := sc.LFSaw{
+		Freq:   sc.C(0.4),
+		Iphase: sc.C(0),
+	}.Rate(sc.AR).MulAdd(sc.C(24), saw2).Midicps()
 
-		sin = sc.SinOsc{
-			Freq:  saw1,
-			Phase: sc.C(0),
-		}.Rate(sc.AR).Mul(sc.C(0.04))
-	)
+	sin := sc.SinOsc{
+		Freq:  saw1,
+		Phase: sc.C(0),
+	}.Rate(sc.AR).Mul(sc.C(0.04))
+
 	return sc.Out{
 		Bus: sc.C(0),
 		Channels: sc.Comb{
